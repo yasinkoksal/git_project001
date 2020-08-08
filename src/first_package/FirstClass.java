ce247cd33437f0111901dcc663897a1f66265be8
yasin koksal <yaasinkoksal@gmail.com>
2020-08-08 12:46:46 -0500

    My local 'yasin' branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -5,6 +5,7 @@ public class FirstClass {
 	public static void main(String[] args) {
 		System.out.println("Our first commit from the local repo");
 		System.out.println("Our second commit from local repository");
+		System.out.println("Now I am on another branch 'yasin'");
 
 	}
 }
