986f9727b4462dcee13a06c86adb53637f312325
yasin koksal <yaasinkoksal@gmail.com>
2020-08-08 13:32:44 -0500

    last commit from master branch

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -6,6 +6,8 @@ public class FirstClass {
 		System.out.println("Our first commit from the local repo");
 		System.out.println("Our second commit from local repository");
 		System.out.println("Now I am on another branch 'yasin'");
+		System.out.println("I will try to resolve a confilict");
 
+		System.out.println("This line will result a second conflict");
 	}
 }
