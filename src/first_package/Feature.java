f4f35903ca4fbbd21f527e267a1db5878b03a96f
yasin koksal <yaasinkoksal@gmail.com>
2020-08-08 15:06:09 -0500

    thr third commit from local master

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -7,7 +7,7 @@ public class Feature {
 		System.out.println("My first commit to feature branch");
 		System.out.println("My first commit from remote master branch");
 		System.out.println("My second commit from the remote master branch");
-		
+		System.out.println("this is the third commit from remote master branch");
 		
 
 		// TODO Auto-generated method stub
