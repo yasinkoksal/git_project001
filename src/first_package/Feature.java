616db16e5e89e0068be3b58e34045aab790a2273
yasinkoksal <69401298+yasinkoksal@users.noreply.github.com>
2020-08-08 14:51:35 -0500

    Update Feature.java
    I have committed first changes from the remote master branch

--- src/first_package/Feature.java
+++ src/first_package/Feature.java
@@ -5,6 +5,8 @@ public class Feature {
 	public static void main(String[] args) {
 
 		System.out.println("My first commit to feature branch");
+		System.out.println("My first commit from remote master branch");
+		
 
 		// TODO Auto-generated method stub
 
