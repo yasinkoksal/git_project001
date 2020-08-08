0c5c6ba22607deb89a178d7e27d962ac1e97210b
yasin koksal <yaasinkoksal@gmail.com>
2020-08-08 11:32:10 -0500

    My second commit from git repository

--- src/first_package/FirstClass.java
+++ src/first_package/FirstClass.java
@@ -4,6 +4,7 @@ public class FirstClass {
 
 	public static void main(String[] args) {
 		System.out.println("Our first commit from the local repo");
+		System.out.println("Our second commit from local repository");
 
 	}
 }
